0294b2f1768259969458704f189eae475d01787c
VicTCB <victcbgaming@gmail.com>
2026-05-20 09:52:29 +0800

    Added  Placeholder Assets
    Added Placeholder Assets

--- automata/Scripts/User/user.gd
+++ automata/Scripts/User/user.gd
@@ -1,23 +1,28 @@
 extends CharacterBody3D
 
-
 const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+const MOUSE_SENSITIVITY = 0.002
 
+@onready var camera = $Camera3D
+@onready var interact_ray = $Camera3D/RayCast3D
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		rotate_y(-event.relative.x * MOUSE_SENSITIVITY)
+		camera.rotate_x(-event.relative.y * MOUSE_SENSITIVITY)
+		camera.rotation.x = clamp(camera.rotation.x, -PI/2, PI/2)
+
+func _physics_process(delta):
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var input_dir = Input.get_vector("Movement_Left", "Movement_Right", "Movement_Forward", "Movement_Backward")
+	
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
@@ -26,3 +31,15 @@ func _physics_process(delta: float) -> void:
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+
+	if Input.is_action_just_pressed("User_Interact"):
+		try_interact()
+
+func try_interact():
+	if interact_ray.is_colliding():
+		var target = interact_ray.get_collider()
+		
+		if target.has_method("on_interact"):
+			target.on_interact()
+		else:
+			pass
